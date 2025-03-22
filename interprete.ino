f7147a30dd34ce401d246be020448202ac0a35c9
Louis Blonde <louis.blonde24@gmail.com>
2025-03-22 02:15:33 +0100

    new models

--- interprete.ino
+++ interprete.ino
@@ -1,31 +1,114 @@
 #include "BluetoothSerial.h";
-//#include <FastLED.h>;
-//#define NUM_LEDS 1;
-//#define DATA_PIN 3;
+#include <FastLED.h>;
+#define NUM_LEDS 300
+#define DATA_PIN 4
+int to_do = 0;
+int color1[3];
+int color2[3];
+int delayer = 10;
+int brightness = 0;
 
-//CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];
 BluetoothSerial SerialBT;
 const int ledPin = 2;
+
+void lb_reset() {
+  to_do = 0;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+}
+void lb_all_leds(int red, int green, int blue) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(red, green, blue);
+    delay(delayer);
+    FastLED.show();
+  }
+}
+void lb_two_colors(int space) {
+  lb_all_leds(color1[0], color1[1], color1[2]);
+  delay(space);
+  lb_all_leds(color2[0], color2[1], color2[2]);
+  delay(space);
+}
+void lb_peer() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (i % 2 == 0) {
+      leds[i] = CRGB(color1[0], color1[1], color1[2]);
+    } else {
+      leds[i] = CRGB(color2[0], color2[1], color2[2]);
+    }
+    FastLED.show();
+    delay(delayer);
+  }
+}
+void lb_color_fade(int space) {
+  FastLED.setBrightness(0);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(color1[0], color1[1], color1[2]);
+  }
+  FastLED.show();
+  for (int i = 0; i < brightness; i++) {
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(40);
+  }
+  delay(5000);
+  for (int i = brightness; i > 0; i--) {
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(40);
+  }
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(color2[0], color2[1], color2[2]);
+  }
+  FastLED.show();
+  for (int i = 0; i < brightness; i++) {
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(40);
+  }
+  delay(5000);
+  for (int i = brightness; i > 0; i--) {
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(40);
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
-  SerialBT.begin("ESP32-Bluetooth");
+  SerialBT.begin("LEDs Lydka");
   pinMode(ledPin, OUTPUT);
-  //FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-  //leds[0] = CRGB(255, 0, 0);
-  //FastLED.show();
-  //delay(500);
-  //leds[0] = CRGB(0, 0, 0);
-  //FastLED.show();
-  //delay(500);
   if (SerialBT.available()) {
-    char c = SerialBT.read();
-    if (c == '1') {
+    FastLED.setBrightness(10);
+    brightness = 10;
+    char info = SerialBT.read();
+    if (info == '1') {
       digitalWrite(ledPin, HIGH);
-    } else if (c == '0') {
+      to_do = 3;
+      color1[0] = 0;
+      color1[1] = 128;
+      color1[2] = 128;
+      color2[0] = 255;
+      color2[1] = 50;
+      color2[2] = 140;
+    } else if (info == '0') {
       digitalWrite(ledPin, LOW);
+      lb_reset();
     }
+  }
+  if (to_do == 1) {
+    lb_two_colors(1000);
+  } else if (to_do == 2) {
+    lb_peer();
+    to_do = 0;
+  } else if (to_do == 3) {
+    lb_color_fade(1000);
   }
 }
